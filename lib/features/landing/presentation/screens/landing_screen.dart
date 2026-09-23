68e74ca331bacd4f8ea5f4b796da4d8931d0edb1
Mohamed solaiman <mohamedsolaiman708@gmail.com>
2026-09-23 08:22:28 +0300

    dn

--- lib/features/landing/presentation/screens/landing_screen.dart
+++ lib/features/landing/presentation/screens/landing_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:url_launcher/url_launcher.dart';
 import '../../../../core/theme/app_theme.dart';
 import '../../../admin/domain/enums/service_type.dart';
 
